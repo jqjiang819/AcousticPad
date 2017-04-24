abaff2949f7d1b4f4fffe47967552483b47a0dee
jqjiang <jjqzy@foxmail.com>
2017-04-24 09:56:11 +0800

    main&levddetect: general fix
    - main: add DBG parameters;fix levd code
    - levddetect: add DBG parameters

--- src/levddetect.m
+++ src/levddetect.m
@@ -9,8 +9,9 @@ function [ data_out, ext_out ] = levddetect( data_in, s_init, ext_in )
 %  than the threshold, we add it to the extreme list. To estimate the 
 %  value of the static vector by calculate the average of the last two 
 %  extremes since the dynamic vector has a trace similar to circles. 
+    
     if ~exist('s_init','var')
-        levd.s(1) = 0;
+        levd.s(1) = data_in(1);
     else
         levd.s(1) = s_init;
     end
@@ -28,6 +29,8 @@ function [ data_out, ext_out ] = levddetect( data_in, s_init, ext_in )
     if size(data_in,2) > 1
         error('levddetect: input data can only be a one-dimension array');
     end
+    global DATA_DBG_EXTS;
+    DATA_DBG_EXTS.cnt = DATA_DBG_EXTS.cnt + 1;
     for i = 1:length(data_in)
         % get local extremes
         switch isminmax(data_in,i)
@@ -35,27 +38,40 @@ function [ data_out, ext_out ] = levddetect( data_in, s_init, ext_in )
                 if strcmp(levd.ext(2).type,'init')
                     levd.ext(2).type = 'min';
                     levd.ext(2).data = data_in(i);
+                    DATA_DBG_EXTS.v = [DATA_DBG_EXTS.v;data_in(i)];
+                    DATA_DBG_EXTS.i = [DATA_DBG_EXTS.i;i+240*DATA_DBG_EXTS.cnt];
                 elseif strcmp(levd.ext(2).type,'min') && (data_in(i) < levd.ext(2).data)
                     levd.ext(2).data = data_in(i);
-                elseif strcmp(levd.ext(2).type,'max')
+                    DATA_DBG_EXTS.v(end) = data_in(i);
+                    DATA_DBG_EXTS.i(end) = i+240*DATA_DBG_EXTS.cnt;
+                elseif strcmp(levd.ext(2).type,'max') && (abs(data_in(i) - levd.ext(2).data) > 2e5)
                     levd.ext(1) = levd.ext(2);
                     levd.ext(2).type = 'min';
                     levd.ext(2).data = data_in(i);
+                    DATA_DBG_EXTS.v = [DATA_DBG_EXTS.v;data_in(i)];
+                    DATA_DBG_EXTS.i = [DATA_DBG_EXTS.i;i+240*DATA_DBG_EXTS.cnt];
                 end
             case 1
                 if strcmp(levd.ext(2).type,'init')
                     levd.ext(2).type = 'max';
                     levd.ext(2).data = data_in(i);
+                    DATA_DBG_EXTS.v = [DATA_DBG_EXTS.v;data_in(i)];
+                    DATA_DBG_EXTS.i = [DATA_DBG_EXTS.i;i+240*DATA_DBG_EXTS.cnt];
                 elseif strcmp(levd.ext(2).type,'max') && (data_in(i) > levd.ext(2).data)
                     levd.ext(2).data = data_in(i);
-                elseif strcmp(levd.ext(2).type,'min')
+                    DATA_DBG_EXTS.v(end) = data_in(i);
+                    DATA_DBG_EXTS.i(end) = i+240*DATA_DBG_EXTS.cnt;
+                elseif strcmp(levd.ext(2).type,'min') && (abs(data_in(i) - levd.ext(2).data) > 2e5)
                     levd.ext(1) = levd.ext(2);
                     levd.ext(2).type = 'max';
                     levd.ext(2).data = data_in(i);
+                    DATA_DBG_EXTS.v = [DATA_DBG_EXTS.v;data_in(i)];
+                    DATA_DBG_EXTS.i = [DATA_DBG_EXTS.i;i+240*DATA_DBG_EXTS.cnt];
                 end
         end
         % calculate static vector
-        levd.s(i+1) = 0.9*levd.s(i)+0.1*getextmean(levd.ext);
+        % levd.s(i+1) = 0.9*levd.s(i)+0.1*getextmean(levd.ext);
+        levd.s(i+1) = getstaticvec(data_in,levd.s,i,levd.ext);
     end
     ext_out = levd.ext;
     data_out = levd.s(2:end)';
@@ -99,4 +115,15 @@ function out = getextmean(ext)
         ise.out = (ext(1).data+ext(2).data)/2;
     end
     out = ise.out;
+end
+
+function out = getstaticvec(data,s,i,ext)
+%GETSTATICVEC
+    extmean = getextmean(ext);
+    if extmean ~= 0
+        vec.out = 0.9*s(i) + 0.1*extmean;
+    else
+        vec.out = 0.9*data(i);
+    end
+    out = vec.out;
 end
--- src/main.m
+++ src/main.m
@@ -1,16 +1,24 @@
 % init
-clear all;
+clear;
 vars.cod.time = 0;
 vars.cic.dbuf_i = zeros(17,3);
 vars.cic.dbuf_q = zeros(17,3);
 vars.cic.ibuf_i = zeros(1,3);
 vars.cic.ibuf_q = zeros(1,3);
-vars.levd.ext = utilities.initext();
-vars.levd.s_init = 0;
+vars.levd.ext_i = utilities.initext();
+vars.levd.s_init_i = 0;
+vars.levd.ext_q = utilities.initext();
+vars.levd.s_init_q = 0;
 s_i = [];
 s_q = [];
 vec_i = [];
 vec_q = [];
+DATA_DBG_i = [];
+DATA_DBG_q = [];
+global DATA_DBG_EXTS;
+DATA_DBG_EXTS.v = [];
+DATA_DBG_EXTS.i = [];
+DATA_DBG_EXTS.cnt = 0;
 % get audio data
 snd_data = sndcnvrt('../res/record.pcm');
 for i = 1:size(snd_data,2)
@@ -21,20 +29,24 @@ for i = 1:size(snd_data,2)
             = cicdecim(data_i,vars.cic.dbuf_i,vars.cic.ibuf_i);
     [data_cic_q,vars.cic.dbuf_q,vars.cic.ibuf_q] ...
             = cicdecim(data_q,vars.cic.dbuf_q,vars.cic.ibuf_q);
+    DATA_DBG_i = [DATA_DBG_i;data_cic_i];
+    DATA_DBG_q = [DATA_DBG_q;data_cic_q];
     % levd detection
-    [s_i_tmp,vars.levd.ext] = levddetect(data_cic_i,vars.levd.s_init,vars.levd.ext);
-    [s_q_tmp,vars.levd.ext] = levddetect(data_cic_q,vars.levd.s_init,vars.levd.ext);
-    s_i = [s_i;s_i_tmp];
-    s_q = [s_q;s_q_tmp];
+    [s_tmp_i,vars.levd.ext_i] = levddetect(data_cic_i,vars.levd.s_init_i,vars.levd.ext_i);
+    vars.levd.s_init_i = s_tmp_i(end);
+    [s_tmp_q,vars.levd.ext_q] = levddetect(data_cic_q,vars.levd.s_init_q,vars.levd.ext_q);
+    vars.levd.s_init_q = s_tmp_q(end);
+    s_i = [s_i;s_tmp_i];
+    s_q = [s_q;s_tmp_q];
     % calculation
-    vec_i_tmp = data_cic_i - s_i_tmp;
-    vec_q_tmp = data_cic_q - s_q_tmp;
-    vec_i = [vec_i;vec_i_tmp];
-    vec_q = [vec_q;vec_q_tmp];
+    vec_tmp_i = data_cic_i - s_tmp_i;
+    vec_tmp_q = data_cic_q - s_tmp_q;
+    vec_i = [vec_i;vec_tmp_i];
+    vec_q = [vec_q;vec_tmp_q];
 end
 % test
-pha = phase(vec_i+1j*vec_q);
-len = -pha/(2*pi)*1.8889;
-len = len-len(1);
+pha = phase(vec_q+1j*vec_i);
+len = pha/(2*pi)*1.8889;
+len = (len-len(1))/2;
 t=1/3000:1/3000:length(len)/3000;
 plot(t,len);
