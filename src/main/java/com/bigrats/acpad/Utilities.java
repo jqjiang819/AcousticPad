5a5c318b68a36aa44a5639037b49c21fbcd5691a
jqjiang <jjqzy@foxmail.com>
2017-04-30 14:01:28 +0800

    Utilities: add sndcnvrt (loadRes remains unfilled)

--- src/main/java/com/bigrats/acpad/Utilities.java
+++ src/main/java/com/bigrats/acpad/Utilities.java
@@ -9,6 +9,28 @@ import com.bigrats.acpad.structs.CodData;
  */
 public class Utilities {
 
+    /**** SNDCNVRT ****/
+
+    private static double[] loadRes(String respath) {
+        /*** This remains to be filled ***/
+        return new double[1];
+    }
+
+    public static double[][] sndcnvrt(String respath) {
+        int frameSize = Params.FRAME_SIZE;
+        double[] pcmdata = loadRes(respath);
+
+        double[][] data = new double[pcmdata.length/frameSize][frameSize];
+
+        if (pcmdata.length % frameSize != 0) {
+            throw new IllegalArgumentException("sndcnvrt: resource length error.");
+        }
+        for (int i = 0; i < (pcmdata.length/frameSize); i++) {
+            System.arraycopy(pcmdata,frameSize*i,data[i],0,frameSize);
+        }
+
+        return data;
+    }
 
 
 
