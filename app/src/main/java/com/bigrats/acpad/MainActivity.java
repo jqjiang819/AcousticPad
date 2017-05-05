951d2f4da66bb1cbcd98a741e34aebfc7a393304
jqjiang <jjqzy@foxmail.com>
2017-05-05 21:07:42 +0800

    Demo: it works!

--- app/src/main/java/com/bigrats/acpad/MainActivity.java
+++ app/src/main/java/com/bigrats/acpad/MainActivity.java
@@ -1,7 +1,5 @@
 package com.bigrats.acpad;
 
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,14 +9,11 @@ import android.widget.TextView;
 import com.bigrats.acpadlib.AcPadHelper;
 
 import java.text.DecimalFormat;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_start = null;
     TextView tv_dist = null;
-    Handler hdl_update = null;
-    Timer timer = null;
+    AcPadHelper acPadHelper = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,63 +35,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Button btn = (Button) v;
-                double value = 0;
                 if (!btn.getContentDescription().equals("STOP")) {
                     btn.setText("STOP");
                     btn.setTextColor(getColor(R.color.colorAccent));
-                    tv_dist.setText("1.00");
                     btn.setContentDescription("STOP");
-                    new AcPadHelper("LEVD").run();
-//                    hdl_update = new Handler() {
-//                        @Override
-//                        public void handleMessage(Message msg) {
-//                            switch (msg.what) {
-//                                case 0:
-//                                    Bundle bundle = msg.getData();
-//                                    double dist = bundle.getDouble("dist", 0);
-//                                    DecimalFormat df = new DecimalFormat("#0.00");
-//                                    tv_dist.setText(df.format(dist));
-//                                    break;
-//                            }
-//                            super.handleMessage(msg);
-//                        }
-//                    };
-//                    new Thread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            timer = new Timer();
-//                            TimerTask task = new TimerTask() {
-//                                double value = 0;
-//                                @Override
-//                                public void run() {
-//                                    Message msg = hdl_update.obtainMessage();
-//                                    Bundle bundle = new Bundle();
-//                                    bundle.putDouble("dist",(value = getDistValue(value)));
-//                                    msg.what = 0;
-//                                    msg.setData(bundle);
-//                                    hdl_update.sendMessage(msg);
-//                                }
-//                            };
-//                            timer.schedule(task,0,100);
-//                        }
-//                    }).start();
-
+                    acPadHelper = new AcPadHelper("LEVD") {
+                        @Override
+                        public void onDataReceive(double[] data) {
+                            DecimalFormat df = new DecimalFormat("#0.00");
+                            tv_dist.setText(df.format(data[data.length-1]));
+                            super.onDataReceive(data);
+                        }
+                    };
+                    acPadHelper.run();
                 } else {
                     btn.setText("START");
                     btn.setTextColor(getColor(R.color.colorPrimaryDark));
                     tv_dist.setText("0.00");
                     btn.setContentDescription("START");
-//                    timer.cancel();
+                    acPadHelper.stop();
                 }
             }
         });
-    }
-
-    private double getDistValue(double init_value) {
-        double value = init_value;
-        for (int i = 0; i < 20; i++) {
-            value += i * 0.01;
-        }
-        return value;
     }
 }
